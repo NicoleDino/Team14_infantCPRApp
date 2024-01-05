db57ea01d4d2a03967cd2f7dfe62f8746e392776
NicoleDino <qnradino@tip.edu.ph>
2024-01-05 18:36:00 +0800

    Updated More.js
    Minor modifications added

--- screens/More.js
+++ screens/More.js
@@ -20,17 +20,17 @@ function More({ navigation }) {
     <View style={styles.container}>
       <Text style={styles.title}>Additional Resources:</Text>
       <TouchableOpacity onPress={() => handleOpenLink('https://raisingchildren.net.au/newborns/safety/cpr/cpr-for-babies')}>
-        <Text style={styles.link}>CPR for babies under 12 months: in pictures</Text>
+        <Text style={styles.link}>🍼 CPR for babies under 12 months: in pictures</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => handleOpenLink('https://www.redcross.org/take-a-class/cpr/performing-cpr/child-baby-cpr')}>
-        <Text style={styles.link}>Child & Baby CPR - Red Cross</Text>
+        <Text style={styles.link}>👶 Child & Baby CPR - Red Cross</Text>
       </TouchableOpacity>
       <TouchableOpacity onPress={() => handleOpenLink('https://www.youtube.com/watch?v=tK-gwp4dPmw')}>
-        <Text style={styles.link}>Additional Infant CPR Video</Text>
+        <Text style={styles.link}>🎥 Additional Infant CPR Video Tutorials/Trainings</Text>
       </TouchableOpacity>
       <Text style={styles.title}>Feedback Form:</Text>
       <TouchableOpacity onPress={handleOpenFeedbackForm}>
-        <Text style={styles.link}>Provide Feedback - add link to form</Text>
+        <Text style={styles.link}>✉️ Provide Feedback - add link to feedback form</Text>
       </TouchableOpacity>
       <TouchableOpacity style={styles.backButton} onPress={handleBackToDashboardPress}>
         <Text style={styles.backButtonText}>Back to Dashboard</Text>
@@ -42,20 +42,22 @@ function More({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    padding: 20,
     backgroundColor: '#FCE4EC',
   },
   title: {
     fontSize: 20,
     fontWeight: 'bold',
     marginBottom: 10,
+    marginTop: 10,
     color: '#FF7FAA',
+    textAlign: 'center'
   },
   link: {
     fontSize: 16,
-    color: '#4285F4', 
+    color: 'gray',
     marginBottom: 10,
+    textAlign: 'left',
   },
   backButton: {
     backgroundColor: '#FF7FAA',
